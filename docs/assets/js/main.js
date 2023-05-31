bee1eb20dfa4c4611bf0cf2323e35726c30f9da7
Bing Hong <1902164@sit.singaporetech.edu.sg>
2023-05-31 22:30:59 +0800

    added render for card

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -178,6 +178,28 @@
   });
 
   /**
+   * Modal
+    */
+  const exampleModal = document.getElementById('exampleModal')
+  if (exampleModal) {
+    exampleModal.addEventListener('show.bs.modal', event => {
+      // Button that triggered the modal
+      const button = event.relatedTarget
+      // Extract info from data-bs-* attributes
+      const task = button.getAttribute('data-bs-whatever')
+      // If necessary, you could initiate an Ajax request here
+      // and then do the updating in a callback.
+
+      // Update the modal's content.
+      const modalTitle = exampleModal.querySelector('.modal-title')
+      const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+      modalTitle.textContent = `Modify Task (Previous task: ${task})`
+      modalBodyInput.value = recipient
+    })
+  }
+
+  /**
    * Portfolio details slider
    */
   new Swiper('.portfolio-details-slider', {
